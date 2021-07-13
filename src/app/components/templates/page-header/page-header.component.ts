2adfa832ba704c5b94c3edc16eff620456ffc29b
Thiago Kassabian <thiago.kassabian@gmail.com>
2021-07-13 16:48:47 -0300

    refino nos estilos

--- src/app/components/templates/page-header/page-header.component.ts
+++ src/app/components/templates/page-header/page-header.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
 	selector: 'page-header',
 	templateUrl: './page-header.component.html',
 	styleUrls: ['./page-header.component.scss'],
 })
-export class PageHeaderComponent implements OnInit {
+export class PageHeaderComponent {
 	@Output('openDialog') openDialog: EventEmitter<any> = new EventEmitter();
 	@Output('applyFilter') applyFilter: EventEmitter<Event> = new EventEmitter();
 
 	constructor() {}
-
-	ngOnInit(): void {}
 
 	openForm = () => {
 		this.openDialog.emit();
